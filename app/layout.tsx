bfb0ca1bd7bc0fae0c22afe56fc677825060cf59
sushwesinwin <sushwesinw@gmail.com>
2026-05-25 20:50:50 +0630

    fix hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
